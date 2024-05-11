dfb171d8f9d9ad70ae132ed887fb1d10ae0c499b
Muhammad Haris <harislive07@gmail.com>
2024-05-11 14:26:13 +0500

    Show page and Job page Componet

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,4 @@
 <div class="mb-4 rounded-md border border-slate-300 bg-white p-4 shadow-sm">
 {{$slot}}
 </div>
+
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
+route::get('',fn()=> route('jobs.index'));
+
 Route::resource('jobs',JobController::class)
-->only(['index']);
+->only(['index','show']);
